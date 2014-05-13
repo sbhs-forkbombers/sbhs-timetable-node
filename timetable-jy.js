65b71d2a23ba81725b63590a4fee38a6aad490e6
gnustomp <lolz836@gmail.com>
2014-05-13 20:07:46 +1000

    Reimplement timetable.coffee, add things to test.
    I _believe_ that this is what timetable.coffee is doing. With 100% less
    trycatching, more extensibility, better output, and IPv6 support.
    Too bad it's not coffeescript.

---
+++ timetable-jy.js
@@ -0,0 +1,53 @@
+/*jshint curly: true, devel: true, indent: 4, latedef: true, quotmark: single, undef: true, unused: true, strict: true, trailing: true */
+/*global require*/
+var http = require('http'),
+	fs = require('fs');
+
+function onRequest(req, res) {
+	/*jshint validthis: true*/
+	'use strict';
+	console.log('[' + this.name + ']', req.method, req.url);
+	
+	if (req.url === '/') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		fs.createReadStream('construction.html').pipe(res);
+	} else if (req.url === '/belltimes.js') {
+		res.writeHead(200, {
+			'Content-Type': 'application/javascript'
+		});
+		fs.createReadStream('belltimes.js').pipe(res);
+	} else if (req.url.match('/.*[.]css$') && fs.existsSync(req.url.slice(1))){
+		res.writeHead(200, {
+			'Content-Type': 'text/css'
+		});
+		fs.createReadStream(req.url.slice(1)).pipe(res);
+	} else {
+		res.writeHead(404, {
+			'Content-Type': 'text/html'
+		});
+		fs.createReadStream('404.html').pipe(res);
+	}
+}
+
+function onListening() {
+	/*jshint validthis: true*/
+	'use strict';
+	console.log('[' + this.name + '] Listening at http://' + this.address().address + ':' + this.address().port + '/');
+}
+
+var ipv4server = http.createServer(),
+	ipv6server = http.createServer();
+
+ipv4server.name = 'ipv4server';
+ipv6server.name = 'ipv6server';
+
+ipv4server.on('request', onRequest);
+ipv6server.on('request', onRequest);
+
+ipv4server.on('listening', onListening);
+ipv6server.on('listening', onListening);
+
+ipv4server.listen(8080, '127.0.0.1');
+ipv6server.listen(8080, '::1');
