0c166024cf2d58296397e3350546dd43af37e50f
gnustomp <lolz836@gmail.com>
2014-06-07 14:20:48 +1000

    *.js: blah

--- script/notices.js
+++ script/notices.js
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*jshint latedef:nofunc*/
 function handleTopPane() {
 	'use strict';
 	var entry, list, today = new Date();
