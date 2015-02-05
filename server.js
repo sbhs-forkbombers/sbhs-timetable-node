11626a056e652dab7709fc28229bf07ea5fb0129
keepcalm444 <keepcalm444@gmail.com>
2015-02-05 23:21:12 +1100

    Move all compression and response-related tasks out of server.js
    Part 1 of issue #135

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* SBHS-Timetable-Node: Countdown and timetable all at once (NodeJS app).
- * Copyright (C) 2014 James Ye, Simon Shields
+ * Copyright (C) 2014-2015 James Ye, Simon Shields
  *
  * This file is part of SBHS-Timetable-Node.
  *
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* globals sessions */
+require('./lib/es6.js');
 process.env.TZ = 'Australia/Sydney';
 var all_start = Date.now();
 console.log('[core] Loading...');
@@ -35,15 +36,12 @@ var fs = require('fs'),
 	config = require('./config.js'),
 	variables = require('./variables.js'),
 	schoolday = require('./lib/schoolday.js'),
+	files = require('./lib/files.js');
 	colours = require('./lib/colours.js');
 
 /* Variables */
 var	IPV6 = config.ipv6,
 	NOHTTP = config.nohttp,
-	RELEASE = variables.RELEASE,
-	MINIFY = variables.MINIFY,
-	REL_RV = variables.REL_RV,
-	DEBUG = variables.DEBUG,
 	SOCKET = config.socket,
 	IPV4_IP = config.ipv4_ip,
 	IPV6_IP = config.ipv6_ip,
@@ -52,6 +50,12 @@ var	IPV6 = config.ipv6,
 	lessParser = new less.Parser(),
 	hmopts = { removeComments: true, removeCommentsFromCDATA: true, collapseWhitespace: true },
 	index_cache, timetable_cache, ipv4, ipv6, socket;
+
+/* Globals */
+global.RELEASE = variables.RELEASE;
+global.MINIFY = variables.MINIFY;
+global.REL_RV = variables.REL_RV;
+global.DEBUG = variables.DEBUG;
 
 if (process.platform == 'win32') {
 	SOCKET = false; // Here's a nickel, kid. Get yourself a better OS.
@@ -68,6 +72,7 @@ if (!RELEASE) {
 		var NO_GIT = true; // If the git revision can't be found (e.g. repo wasn't cloned) we use the git release version
 	}
 }
+
 fs.writeFile('.reload', '0');
 
 console.log('[core] Initialised in in ' + (Date.now() - all_start) + 'ms');
@@ -78,11 +83,6 @@ var jade_opts = {
 	compileDebug: DEBUG
 };
 
-function serverError() {
-	/* Returns the 500 Internal Server Error page */
-	'use strict';
-	return (Math.random() < 0.9 ? fs.createReadStream('static/500.html') : fs.createReadStream('static/500.8.html'));
-}
 
 function compile_jade(path) {
 	/* Compiles jade templates into HTML */
@@ -94,7 +94,7 @@ function compile_jade(path) {
 	} catch (e) {
 		console.error('[core_emerg] Failed to compile jade "'+path+'"!!! Stack trace:');
 		console.error(e.stack);
-		return serverError;
+		return null;
 	}
 }
 
@@ -104,6 +104,8 @@ function compile_less(path, colour, callback) {
 	lessParser.parse(fs.readFileSync(path, { encoding: 'utf8' }), function(e, tree) {
 		if (e) {
 			callback(500,'text/html', serverError());
+			console.error('[core_emerg] Failed to compile less ' + path + ' (colour: ' + colour + ')');
+			console.error(e);
 		}
 		else {
 			callback(200, 'text/css', tree.toCSS({ compress: MINIFY }));
@@ -119,11 +121,11 @@ function cache_index() {
 	console.log('[core] Caching index/timetable pages... (hangup to re-cache)');
 	var jade_comp = Date.now();
 	index_cache = compile_jade('dynamic/index.jade');
-	if (index_cache == serverError) {
+	if (index_cache === null) {
 		console.error('[core_emerg] Encountered an error while caching index page. Fix errors, and then hangup to reload.');
 	}
 	timetable_cache = compile_jade('dynamic/timetable.jade');
-	if (timetable_cache == serverError) {
+	if (timetable_cache === null) {
 		console.error('[core_emerg] Encountered an error while caching timetable page. Fix errors, and then hangup to reload.');
 	}
 	console.log('[core] Index and Timetable pages cached in ' + (Date.now() - jade_comp) + 'ms');
@@ -158,58 +160,6 @@ function cleanSessions() {
 	console.log('[core] Wrote ' + Object.keys(global.sessions).length + ' sessions to disk');
 }
 
-function compressText(req, text, res, cb) {
-	'use strict';
-	if (typeof cb !== 'function') {
-		return;
-	}
-	if (res._headerSent) {
-		cb(undefined,text);
-	}
-	if ('accept-encoding' in req.headers) {
-		var encs = req.headers['accept-encoding'].replace(/q=\d\.\d/, '').split(/, ?/);
-		if (encs.indexOf('gzip') != -1) {
-			if (DEBUG) {
-				console.log('[compress_debug] using gzip compression');
-			}
-			res.setHeader('Content-Encoding', 'gzip');
-			zlib.gzip(text, cb);
-			return;
-		}
-		else if (encs.indexOf('deflate') != -1) {
-			if (DEBUG) {
-				console.log('[compress_debug] using deflate compression');
-			}
-			res.setHeader('Content-Encoding', 'deflate');
-			zlib.deflate(text, cb);
-			return;
-		}
-	}
-	cb(text);
-}
-
-function pipeCompress(req, file, res) {
-	'use strict';
-	var result = fs.createReadStream(file);
-	if ('accept-encoding' in req.headers) {
-		var encs = req.headers['accept-encoding'].replace(/q=\d\.\d/, '').split(/, ?/);
-		if (encs.indexOf('gzip') != -1) {
-			if (DEBUG) {
-				console.log('[compress_debug] using gzip compression');
-			}
-			res.setHeader('Content-Encoding', 'gzip');
-			result = result.pipe(zlib.createGzip());
-		}
-		else if (encs.indexOf('deflate') != -1) {
-			if (DEBUG) {
-				console.log('[compress_debug] using deflate compression');
-			}
-			res.setHeader('Content-Encoding', 'deflate');
-			result = result.pipe(zlib.createDeflate());
-		}
-	}
-	return result;
-}
 
 function exit() {
 }
@@ -265,39 +215,6 @@ process.on('SIGTERM', function() {
 	process.exit(0);
 });
 
-function httpHeaders(res, req, responseCode, contentType, dynamic, tag, headers) {
-	/* Generate HTTP headers, including the response code, content type etc. */
-	'use strict';
-	var date;
-	dynamic = dynamic || false;
-	headers = headers || {};
-	var data = url.parse(req.url);
-	if (!('Set-Cookie' in headers) && 'SESSID' in res && data.pathname == '/login') {
-		console.log('set cookie',res.SESSID);
-		var exp = new Date();
-		exp = new Date(exp.valueOf() + 60*60*24*90*1000);
-		console.log('expire',exp,exp.valueOf());
-		headers['Set-Cookie'] = 'SESSID='+res.SESSID+'; Path=/; Expires=' + exp.toGMTString();
-	}
-	if (dynamic || DEBUG) { // disable caching
-		headers['Cache-Control'] = 'no-cache, must-revalidate';
-		headers.Pragma = 'no-cache';
-		headers.Expires = 'Sat, 26 Jul 1997 05:00:00 GMT';
-	} else if (!dynamic) {
-		date = new Date();
-		date.setYear(date.getFullYear() + 1);
-		headers.Expires = date.toGMTString();
-	}
-
-	if (tag !== null && tag !== undefined) {
-		headers.ETag = tag;
-	}
-	headers['Content-Type'] = contentType + '; charset=UTF-8';
-	headers['X-Powered-By'] = 'Node.js ' + process.version;
-	res.writeHead(responseCode, headers);
-	return res;
-}
-
 function genSessionID() {
 	/* Generate a random session ID */
 	'use strict';
@@ -314,38 +231,6 @@ function getCookies(s) {
 	return res;
 }
 
-function checkFile(file, req, unchanged, changed) {
-	'use strict';
-	var reqHash = '';
-	if ('if-none-match' in req.headers) {
-		reqHash = req.headers['if-none-match'];
-	}
-	etag.file(file, function(err, hash) {
-		if (err !== null) {
-			changed(null); // meh
-		} else if (reqHash !== hash) {
-			changed(hash);
-		} else {
-			unchanged(hash);
-		}
-	});
-}
-
-function checkText(text, req, unchanged, changed) {
-	'use strict';
-	var reqHash = '';
-	if ('if-none-match' in req.headers) {
-		reqHash = req.headers['if-none-match'];
-	}
-	etag.text(text, function(hash) {
-		if (reqHash !== hash) {
-			changed(hash);
-		} else {
-			unchanged(hash);
-		}
-	});
-}
-
 function onRequest(req, res) {
 	/* Respond to requests */
 	/* jshint validthis: true */
@@ -355,27 +240,9 @@ function onRequest(req, res) {
 	filePath = 'static/404.html',
 	that = this;
 	res.on('finish', function() {
-		console.log('[' + that.name + ']', req.method, req.url, '-', res.statusCode, 'in', Date.now()-start + 'ms');
+		console.log('[' + that.name + ']', req.method, req.url, '-', res.statusCode, 'in', Date.now()-start + 'ms - ' + req.headers['user-agent']);
 	});
-	var changed = function(hash) {
-		var pipe = pipeCompress(req, filePath, res);
-		httpHeaders(res, req, 200, contentType, false, hash);
-		pipe.pipe(res);
-	};
-	var dynChanged = function(hash) {
-		compressText(req, target, res, function(a,r) {
-			if (a) {
-				res.setHeader('Content-Encoding', '');
-				r = target;
-			}
-			httpHeaders(res, req, 200, contentType, true, hash);
-			res.end(r);
-		});
-	};
-	var unchanged = function() {
-		res.writeHead(304);
-		res.end();
-	};
+
 	if ('cookie' in req.headers) {
 		var cookies = getCookies(req.headers.cookie);
 		if ('SESSID' in cookies) {
@@ -416,9 +283,8 @@ function onRequest(req, res) {
 			scheme.bg = scheme.fg;
 			scheme.fg = tmp;
 		}
-		if (index_cache == serverError) {
-			httpHeaders(res, req, 500, 'text/html', true);
-			target().pipe(res);
+		if (index_cache === null) {
+			files.err500();
 		} else {
 			compile_less('style/index.less', scheme, function(code, type, less) {
 				target = index_cache({
@@ -439,7 +305,8 @@ function onRequest(req, res) {
 				if (MINIFY) {
 					target = minify(target, hmopts);
 				}
-				checkText(target, req, unchanged, dynChanged);
+				files.contentType('text/html', res);
+				files.respondText(target, req, res);
 			});
 		}
 		//httpHeaders(res, req, (target == serverError ? 500 : 200), 'text/html', true);
@@ -452,67 +319,57 @@ function onRequest(req, res) {
 				if (MINIFY) {
 					target = minify(target, hmopts);
 				}
-				checkText(target, req, unchanged, dynChanged);
+				res.setHeader('Content-Type', 'text/html');
+				files.respondText(target, req, res);
 			});
 		} else {
 			target = timetable_cache({'loggedIn': loggedIn});
 			if (MINIFY) {
 				target = minify(target, hmopts);
 			}
-			checkText(target, req, unchanged, dynChanged);
+			res.setHeader('Content-Type', 'text/html');
+			files.respondText(target, req, res);
 		}
 	} else if (uri.pathname.match('.*config[.]js.*') && fs.existsSync('config_sample.js')) {
 		httpHeaders(res, req, 403, 'text/plain');
 		fs.createReadStream('config_sample.js').pipe(res);
-	} else if (uri.pathname.match('/style/.*[.]css$') && fs.existsSync(uri.pathname.slice(1))) {
+	} else if (uri.pathname.match('/style/.*[.]css$') /*&& fs.existsSync(uri.pathname.slice(1))*/) {
 		/* Style sheets */
-		contentType = 'text/css';
 		filePath = uri.pathname.slice(1);
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname.match('/style/.*[.]less$') && fs.existsSync(uri.pathname.slice(1))) {
 		/* Less style sheets */
 		compile_less(uri.pathname.slice(1), colours.get(uri.query.colour, 'invert' in uri.query), function(rescode, type, css) {
 			if (type === 'text/html') {
-				httpHeaders(res, req, rescode, type);
+				files.fileHeaders(null, res);
+				files.contentType('text/html', res);
 				res.end(css);
 			}
 			else {
-				contentType = type;
-				target = css;
-				checkText(target, req, unchanged, dynChanged);
+				files.contentType(type, res);
+				files.respondText(target, req, res);
 			}
 		});
 	} else if (uri.pathname.match('/script/.*[.]js$') && fs.existsSync(uri.pathname.slice(1))) {
 		/* JavaScript */
-		contentType = 'application/javascript';
-		filePath = uri.pathname.slice(1);
-		checkFile(filePath, req, unchanged, changed);
-	} else if (uri.pathname == '/api/belltimes') {
-		/* Belltimes wrapper */
-		apis.get('belltimes', uri.query, res.SESSID, function(obj) {
-			httpHeaders(res, req, 200, 'application/json', true, obj.etag);
-			res.end(obj.json);
-		});
+		files.respondFile(uri.pathname.slice(1), req, res);
 	} else if (uri.pathname.match('/static/.*[.]jpg|jpeg$') && fs.existsSync(uri.pathname.slice(1))) {
 		/* jpegs */
-		contentType = 'image/jpeg';
 		filePath = uri.pathname.slice(1);
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/favicon.ico') {
 		/* favicon */
-		contentType = 'image/x-icon';
 		filePath = 'static/favicon.ico';
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/static/icon-hires.png' || uri.pathname == '/static/icon-hires.ico' || uri.pathname == '/icon.png') {
 		/* hires icon */
-		contentType = 'image/png';
 		filePath = 'static/icon-hires.png';
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/COPYING') {
 		/* license */
-		contentType = 'text/plain';
 		filePath = 'COPYING';
-		checkFile(filePath, req, unchanged, changed);
+		files.contentType('text/plain', res);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/try_do_oauth') {
 		/* OAuth2 attempt */
 		if ('app' in uri.query) {
@@ -532,37 +389,39 @@ function onRequest(req, res) {
 				console.log('[core_debug] Sending to /mobile_loading!');
 			}
 			auth.getAuthToken(res, uri, function() {
-				httpHeaders(res, req, 302, '', true, null, { 'Location': '/mobile_loading?sessionID='+encodeURIComponent(res.SESSID) });
+				files.textHeaders(res.SESSID, res);
+				res.writeHead(302, {'Location': '/mobile_loading?sessionID='+encodeURIComponent(res.SESSID)});
 				res.end();
 				//httpHeaders(res, req, 200, 'application/json', true);
 				//res.end(JSON.stringify(global.sessions[res.SESSID]));
 			}, false);
 		} else {
 			auth.getAuthToken(res, uri, function() {
-				httpHeaders(res, req, 302, '', true, null, { 'Location': '/' });
+				files.textHeaders(res.SESSID, res);
+				res.writeHead(302, {'Location': '/'});
 				res.end();
 			}, false);
 		}
 	} else if (uri.pathname == '/mobile_loading') {
-		httpHeaders(res, req, 200, 'text/html', true, null);
-		fs.createReadStream('static/appLoading.html').pipe(res);
-	} else if (uri.pathname == '/session_debug' && DEBUG) {
+		files.respondFile('static/appLoading.html', req, res);
+	} else if (uri.pathname == '/session_info' && DEBUG) {
 		/* Session info */
 		httpHeaders(res, req, 200, 'application/json', true);
 		var obj = {};
 		obj[res.SESSID] = global.sessions[res.SESSID];
 		res.end(JSON.stringify(obj));
-	} else if (uri.pathname.match('/api/.*[.]json') && apis.isAPI(uri.pathname.slice(5))) {
+	} else if (uri.pathname.match('/api/.*') && apis.isAPI(uri.pathname.slice(5))) {
 		/* API calls */
 		apis.get(uri.pathname.slice(5), uri.query, res.SESSID, function(obj) {
 			contentType = 'application/json';
 			target = JSON.stringify(obj);
-			checkText(target, req, unchanged, dynChanged);
+			files.contentType(contentType, res);
+			files.respondText(target, req, res);
 		});
 	} else if (uri.pathname == '/logout') {
 		/* Log out */
-		httpHeaders(res, req, 302, 'text/plain', true, null, { 'Location': '/' });
-		res.end('Redirecting...');
+		res.writeHead(302, { 'Location': '/' });
+		res.end();
 		delete global.sessions[res.SESSID].accessToken;
 		delete global.sessions[res.SESSID].refreshToken;
 		delete global.sessions[res.SESSID].accessTokenExpires;
@@ -570,17 +429,13 @@ function onRequest(req, res) {
 	} else if (uri.pathname == '/wat.html') {
 		/* Landing page */
 		filePath = 'static/wat.html';
-		contentType = 'text/html';
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/faq.html') {
 		/* FAQs */
-		filePath = 'static/faq.html';
-		contentType = 'text/html';
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile('static/faq.html');
 	} else if (uri.pathname == '/robots.txt') {
 		filePath = 'static/robots.txt';
-		contentType = 'text/plain';
-		checkFile(filePath, req, unchanged, changed);
+		files.respondFile(filePath, req, res);
 	} else if (uri.pathname == '/reset_access_token') {
 		/* Reset access token */
 		httpHeaders(res, req, 200, 'application/json', true);
@@ -607,8 +462,7 @@ function onRequest(req, res) {
 		serverError().pipe(res);
 	} else {
 		/* 404 everything else */
-		httpHeaders(res, req, 404, 'text/html');
-		fs.createReadStream('static/404.html').pipe(res);
+		files.err404(res);
 	}
 }
 
@@ -626,8 +480,7 @@ function requestSafeWrapper(req, res) {
 		console.error('[core_error] ERROR HANDLING REQUEST: ' + req.url);
 		console.error(e);
 		console.error(e.stack);
-		res.writeHead(500, 'text/html');
-		serverError().pipe(res);
+		files.err500(res);
 	}
 }
 
@@ -652,8 +505,7 @@ if (RELEASE || NO_GIT) {
 	console.log('[core] SBHS-Timetable-Node revision ' + GIT_RV + ' starting server...');
 }
 
-/* index_cache defaults to 500, cache the index page */
-index_cache = serverError;
+/* cache the index page */
 cache_index();
 
 /* Start HTTP servers. */
@@ -686,7 +538,7 @@ if (SOCKET) {
 	socket.listen(socket.path);
 	fs.chmod(socket.path, '777');
 } else if (!SOCKET && NOHTTP) {
-	console.warn('[core_warn] NOHTTP is true, but socket not activated! Disable NOHTTP or make `socket\' true in config.js');
+	console.warn('[core_warn] NOHTTP is true, but socket not activated! Disable NOHTTP or make \'socket\' true in config.js');
 }
 
 setInterval(cleanSessions, 900000); // clean expired sessions every 15 minutes
